2859d469db5d693dc166fbff091a64281ec5ff68
Aguila-dev-2026 <mauriciopulgarcanales@gmail.com>
2026-08-28 05:29:07 +0000

    Quitar encabezado de fecha del tooltip

--- funciones/dashboard_area.py
+++ funciones/dashboard_area.py
@@ -346,6 +346,7 @@ def mostrar_dashboard_area(nombre_area, titulo):
         tickvals=marcas_eje_x,
         ticktext=etiquetas_eje_x,
         tickangle=0,
+        unifiedhovertitle=dict(text=""),
         fixedrange=True,
         automargin=True,
     )
--- funciones/graficos/render.py
+++ funciones/graficos/render.py
@@ -18,10 +18,7 @@ def aplicar_estilo_premium(fig, tipo_grafico, parametro, unidad):
     borde_marcador = paleta_grafico(tema_nativo_oscuro())["borde_marcador"]
     etiqueta_valor = f"%{{y:,.2f}} {unidad}".strip()
     fig.update_traces(
-        hovertemplate=(
-            "<b>%{x|%d/%m/%Y}</b><br>"
-            f"{parametro}: {etiqueta_valor}<extra></extra>"
-        )
+        hovertemplate=f"{parametro}: {etiqueta_valor}<extra></extra>"
     )
     if tipo_grafico == "Barras":
         fig.update_traces(
