7652ed98b415eb7fb7a20872cd13ed5ae3afe019
Aguila-dev-2026 <mauriciopulgarcanales@gmail.com>
2026-08-28 04:11:28 +0000

    Eliminar llamada duplicada de estilos

--- app.py
+++ app.py
@@ -50,7 +50,6 @@ if CONFIGURACION.es_nube and CONFIGURACION.faltantes_base_datos():
 st.title("📊 Dashboard Operacional Planta RILES")
 
 st.sidebar.title("Menú Planta RILES")
-aplicar_detalles_grafico()
 pagina = st.sidebar.radio(
     "Selecciona una sección",
     list(SECCIONES),
